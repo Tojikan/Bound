e8635bf129adeda0fd5550585c667da3ab12392e
Tojikan <andrew.eqian@gmail.com>
2018-02-11 14:55:06 -0500

    Basic Sounds and Death Animation
    Explosion sounds added (Up to 3 at a time). Music added. Death animation and sound added.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BoundEngine;
+
+//Class to handle all other Player things such as death animation and sounds
+public class Player : MonoBehaviour
+{
+    public GameObject deathAnimation;                                      //reference our deathAnimation prefab
+
+    public GameObject instance;
+    private AudioSource deathAudio;                                        //reference to our death Audio Source
+    private void Start()
+    {
+        deathAudio = GetComponent<AudioSource>();
+    }
+
+    //Creates a player death animation and then plays the death sound
+    public void PlayerDeath()
+    {
+
+        //Prevents multiple death animations being spawned if you're hitting multiple colliders
+        //At the same time, created a new bug where if you hit an explosion before an animation finishes, a death animation won't spawn. 
+        //TO DO fix this
+        if (instance == null)
+          {
+             instance = Instantiate(deathAnimation, transform.position, transform.rotation);
+          }
+
+        //Spawn a prefab that does a death animation. 
+        //TO DO refine further. Maybe build it into the player animation? It caused some bugs but might be better to do that. 
+        SoundManager.instance.PlaySingleThree(deathAudio.clip);     
+
+    }
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BoundEngine
+{
+    //manages our sounds and music
+    //Can play music and simultaeneously play two clips at the same time
+    public class SoundManager : MonoBehaviour
+    {
+        public static SoundManager instance = null;                         //Lets us call our sound manager from other scripts
+        public AudioSource efxSource;                                       //effect to be played
+        public AudioSource efxSourceTwo;                                    //second effect to be played
+        public AudioSource efxSourceThree;                                  //third effect to be played
+        public AudioSource musicSource;                                     //music to be played
+
+
+        private void Awake()
+        {
+            //Sets us to be a singleton. If the instance doesn't exist, set it to this. Otherwise destroy
+            if (instance == null)
+                instance = this;
+            else if (instance != this)
+                Destroy(gameObject);
+        }
+
+        //Plays a single clip at a time. 
+        public void PlaySingle(AudioClip clip)
+        {
+            //Set clip
+            efxSource.clip = clip;
+
+            //If we're playing another, stop
+            if (efxSource.isPlaying)
+                efxSource.Stop();
+            //Then play this
+            efxSource.Play();
+        }
+
+        //Second single clip player. Plays another clip
+        public void PlaySingleTwo(AudioClip clip)
+        {
+            //Set clip
+            efxSourceTwo.clip = clip;
+
+            //If we're playing another, stop
+            if (efxSourceTwo.isPlaying)
+                efxSourceTwo.Stop();
+            //Then play this
+            efxSourceTwo.Play();
+        }
+        //Third single clip player. Plays another clip
+        public void PlaySingleThree(AudioClip clip)
+        {
+            //Set clip
+            efxSourceThree.clip = clip;
+
+            //If we're playing another, stop
+            if (efxSourceThree.isPlaying)
+                efxSourceThree.Stop();
+            //Then play this
+            efxSourceThree.Play();
+        }
+
+        //play and set music.
+        public void PlayMusic(AudioClip music)
+        {
+            musicSource.clip = music;
+            musicSource.loop = true;
+            musicSource.Play();
+        }
+    }
+}
