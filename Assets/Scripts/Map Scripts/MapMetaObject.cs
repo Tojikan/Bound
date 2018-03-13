cfb19a3af8bab7b3a7a5ac41544b16351dfc3faf
Abdullah Sahyouni <abdullahsahyouni@gmail.com>
2018-03-13 00:11:57 -0400

    Added new meta data on maps

---
+++ Assets/Scripts/Map Scripts/MapMetaObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BoundMaps {
+
+	[Serializable]
+	public class MapMetaObject {
+
+		public string name;
+		public string fileLocation;
+
+		// Use this for initialization
+		public MapMetaObject (string name, string fileLocation) {
+			this.name = name;
+			this.fileLocation = fileLocation;
+		}
+	}
+}
